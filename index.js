19abd96227dc55e8a3b99f1d094d471ffa2102ca
SandunSenevirathna <sandunsandatharu@gmail.com>
2024-03-26 09:37:24 +0530

    add Constuctor Function and Factory Function

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello Server");
+// Create an Object
+
+/*const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    draw: function(){
+        console.log("Drawing a Circle");
+    },
+};
+*/
+
+
+// Factory Function
+
+function createCircle(radius){
+    return{
+        radius,
+        draw: function(){
+            console.log(`Factory Function | Radius is ${radius} and Drawing a Circle`);
+        },
+    };
+}
+
+const circle = createCircle(1)
+
+// Constuctor Function
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw =  function(){
+        console.log(`Constuctor Function | Radius is ${radius} and Drawing a Circle`);
+    }
+}
+
+const newCircle = new Circle(1)
+
+
+newCircle.draw();
